d7537b7501d2a5ed1c378d0d1a78adfcd9d48194
Zulfikarnaen <h1101241009@student.untan.ac.id>
2026-05-19 22:02:21 +0700

    connect frontend with aws lambda backend

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import BerandaPage from "./pages/BerandaPage";
 import HomeLoggedInPage from "./pages/HomeLoggedInPage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BerandaPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+
         <Route path="/home" element={<HomeLoggedInPage />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
